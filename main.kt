b141a7357ffa233fe41b2290d6615730c6ecf6ab
amriteshgupta <48851357+amriteshgupta@users.noreply.github.com>
2021-09-21 19:34:46 +0530

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,30 @@
+
+import java.util.*
+import java.util.Arrays.*
+
+fun main() {
+    // Write your code
+//val s="Hello You are Learning Kotlin"
+//    val stringlength = s.length
+//    val stringindex = s.get(0)
+//
+//    println("The length of the string is :${stringlength}")
+//    println("The element at index 9: ${s[9]}")
+//
+//    println(stringindex)
+//    val i=9
+//    println("Value is $i")
+//    val newArray:Array<Any> =arrayOf("1,2,3,4")
+//    println(newArray.contentToString())
+//    OR
+//    println(Arrays.toString(newArray))
+
+//var string = ("Kotlin Programs")
+//    val i= length(string)
+
+//    val joey= "How you doin."
+//    println(joey[1.5])
+    var array1=arrayOf(1,3,4.5,"Hello",'c')
+    println(contentToString(array1))
+
+    }
